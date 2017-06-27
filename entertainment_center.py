3ac541e01abda58466b335145c9e5f236150985b
RISHI RAMAWAT <rishi_ramawat@hotmail.com>
2017-06-28 00:43:51 +0530

    entertainment_center now works without OMDb too
    Modified entertainment_center to support people who do not have access
    to OMDb API

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,20 +1,32 @@
 import fresh_tomatoes
 import json
 import media
+import settings
 
-
-with open('movies_list_omdb.json', 'r') as file:
-    json_data = json.load(file)
 
 movies = []  # type: List[media.Movie]
 
-for var in json_data:
-    movie = media.Movie.from_omdb_api(
-        var["imdb_id"],
-        var["trailer_youtube_url"]
-    )
+if settings.OMDB_API_KEY is not None:  # If you have access to OMDb API
+    with open('movies_list_omdb.json', 'r') as file:
+        json_data = json.load(file)
 
-    if movie is not None:
+    for var in json_data:
+        movie = media.Movie.from_omdb_api(
+            var["imdb_id"],
+            var["trailer_youtube_url"]
+        )
+
+        if movie is not None:
+            movies.append(movie)
+
+else:  # If you do not have access to OMDb API (As it is a paid API now)
+
+    with open('movies_list.json', 'r') as file:
+        json_data = json.load(file)
+
+    for movie_data in json_data:
+        movie = media.Movie.from_json(movie_data)
         movies.append(movie)
 
+# Generate & open an HTML page and populate it with all the movies data.
 fresh_tomatoes.open_movies_page(movies)
